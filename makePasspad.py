e172b060f8916762bd29aba219580aaad9017504
John Vilsack <vilsack@gmail.com>
2022-06-28 17:16:55 -0500

    HTML test

--- makePasspad.py
+++ makePasspad.py
@@ -26,8 +26,7 @@ class WordList:
     self.prepare_columns()
 
     # Create actual Grid
-    self.generate_passgrid()
-    
+    self.generate_passgrid()   
     return
 
   # Open wordlist dictated in settings
@@ -36,7 +35,6 @@ class WordList:
     with open(self.config['SETTINGS']['listsdir'] + self.config['SETTINGS']['wordfile'], 'r') as f:
       # Add to object
       self.IMPORTED_WORDLIST = json.load(f)
-
     return
 
   # Using a gridseed means that randomization is deterministic (i.e. can be replicated)
@@ -59,7 +57,6 @@ class WordList:
 
     # Populate object with new values matching total number of values needed
     self.working_wordlist = wordlist[0:TOTAL_CELLS]
-
     return
 
 
@@ -82,17 +79,7 @@ class WordList:
 
     # Populate object with only values needed
     self.generated_columns = result[0:NUMBER_OF_COLUMNS]
-
     return
-
-
-
-
-
-
-
-
-
 
   # Apply the order of the wordlist to the columns we created
   # Order is left to right, top to bottom (e.g. A1, B1, C1, A2, B2, C2, etc.)
@@ -100,61 +87,87 @@ class WordList:
 
     WORDLIST = self.working_wordlist
     COLUMNS = self.generated_columns
-    NUMBER_OF_COLUMNS = int(self.config['SETTINGS']['gridcols'])
-    NUMBER_OF_ROWS = int(self.config['SETTINGS']['gridrows'])
-    TOTAL_CELLS = int(self.config['SETTINGS']['gridcols']) * int(self.config['SETTINGS']['gridrows'])
 
+    print(COLUMNS)
+    print(len(WORDLIST))
 
-    #this_word = 0
     this_column = 0
     this_row = 1
-
     result = {}
 
-    # Iterate over wordlist
-    for this_word in WORDLIST:
-      this_cell = COLUMNS[this_column] + str(this_row).zfill(2)
-      result[this_cell] = this_word
+    i = 0
 
-      # CHANGE COUNTERS
-      # If column iterator isn't above 0:
-      if this_column // NUMBER_OF_COLUMNS == 0:
-        # Move cursor to next column, same row
-        this_column += 1      
-      # If end of columns has been reached
-      else:
-        # Reset Column cursor
-        this_column = 1
-        # Drop the Row cursor down a line
-        this_row += 1 
+    with open('test.txt', 'w') as f:
+      # Iterate over wordlist
+      for this_word in WORDLIST:
+        this_cell = COLUMNS[this_column] + str(this_row).zfill(2)
+        result[this_cell] = this_word
+        f.write('{0}: {1}\n'.format(this_cell, this_word))
+        #print("[{0} {1}] {2}::{3}".format(this_column, this_row, this_cell, this_word))
+        # Compare current column with total columns minus 1 since lists start at 0
+        if this_column // (len(COLUMNS)-1) == 0:
+          # Increment column
+          this_column += 1
+        else:
+          # Reset column
+          this_column = 0
+          # Increment row
+          this_row += 1
+        i += 1
+      f.close()
+    
+    print('{0} words {1} iterations {2} columns {3} rows'.format(len(WORDLIST), i, len(COLUMNS), this_row))
+    print('Expected {0} cells'.format(int(self.config['SETTINGS']['gridcols']) * int(self.config['SETTINGS']['gridrows'])))
 
     self.working_grid = result
-    print('{0} {1}'.format(len(result), len(self.working_wordlist)))
-    print(result)
     return
-
-
-
-
-
-
-
-
-
-
-
-
 
   def makeFinal(self):
     with open(self.config['SETTINGS']['gridjson'], 'w') as f:
       json.dump(self.working_grid, f)
     print('done!')
 
+  def makeHTML(self):
+    GRID = self.working_grid
+    GRIDCOLS = int(self.config['SETTINGS']['gridcols'])
+
+    printCell = 0
+    printRow = 0
+    printCol = 1
+
+    f = open('table.html', 'w')
+    f.write('<html>\n<head>\n')
+    f.write('\t<style>\n')
+    f.write('\t\thtml { font-family: monospace; }\n\t\ttable td { font-size: 9pt; border: 1px solid black; padding: 5px; }\n')
+    f.write('\t</style>\n')
+    f.write('</head>\n<body>\n')
+    f.write('<table>\n\t<tr>\n')
+
+    for cell in GRID:
+      cellValue = format(cell + ' :: ' + GRID[cell])
+      cellHTML = '\t\t<td>' + cellValue + '</td>\n'
+      f.write(cellHTML)
+
+      if printCol // GRIDCOLS == 0:
+        printCol += 1
+      else:
+        f.write('\t</tr>\n\t<tr>\n')
+        printCol = 1
+        printRow += 1
+      printCell += 1
+    f.write('\t</tr>\n</table>\n</body>\n</html>')
+    f.close()
+    print('Grid Written to HTML')
+
 def main():
+  # Generate config values
   config = padConfig.Config()
   config.init_config()
+
+  # Wordlist
   wordlist = WordList(config)
   wordlist.generate_wordlist()
+  wordlist.makeHTML()
   wordlist.makeFinal()
 
 if __name__ == "__main__":
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Features
+* Automatically autofills lists to meet grid size requirements
+  * Added benefit of similar words having multiple coordinates
+* Grid scales infinitely in row and column directions
+* Store decoding grid separate from encryption value
+* Values are easy to decode...even for grandma
+* Values are meaningless
+* Encryption strength as powerful as a one-time-pad
+* Using settings file you can recreate deterministic grid
+* Modular code separates file manipulation from security portions, allowing for easy porting to another language
+* Code only uses standard python libraries for futureproofing
+* Several large lists included for testing and generation
+* Columns replicate well-known "Excel" grid for easier hunting and finding of values
